097d0577d0ed274a5a2a7ccfa7511317817c8273
user <github email address>
2020-10-24 19:11:46 -0400

    commit tree 658c999a4876a7027a88163ba437bd12a731ba86

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
+require 'pry' 
+
 def square_array(array)
+  array_squared = []
+  array.each{|arrays| array_squared << arrays ** 2}
+  array_squared
   # your code here
 end
+
+# def square_array(array)
+#   array.collect! {|arrays| arrays ** 2}
+#   array 
+# end 
+
+
+# numbers = [2,4,6,8]
+
+# square each number
+# squared_numbers = numbers.map {|number| number ** 2}
+
+# print squared_numbers
