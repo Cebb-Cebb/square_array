c5f4bbd883b184f344d5509f2bfc5b633f565733
user <github email address>
2020-10-24 19:12:53 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+require 'pry' 
+
 def square_array(array)
+  array_squared = []
+  array.each{|arrays| array_squared << arrays ** 2}
+  array_squared
   # your code here
 end
+
+# def square_array(array)
+#   array.collect! {|arrays| arrays ** 2}
+#   array 
+# end 
+## mutating an othersise non destructuve iterator to bypass creating a storage variable to hold the new values in
